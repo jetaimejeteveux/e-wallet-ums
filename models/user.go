afd0ed1169bfcf7671e12aea562a90855241a7cb
jetaimejeteveux <akufirman22@gmail.com>
2025-01-08 23:05:30 +0700

    fix: created_at and updated_at not working properly

--- models/user.go
+++ models/user.go
@@ -7,16 +7,16 @@ import (
 )
 
 type User struct {
-	ID          int    `json:"id"`
-	Username    string `json:"username" gorm:"column:username;type:varchar(20)" validate:"required"`
-	Email       string `json:"email" gorm:"column:email;type:varchar(100)" validate:"required"`
-	Password    string `json:"password" gorm:"column:password;type:varchar(72)" validate:"required"`
-	PhoneNumber string `json:"phone_number" gorm:"column:phone_number;type:varchar(15)" validate:"required"`
-	Address     string `json:"address" gorm:"column:address;type:text" `
-	Dob         string `json:"dob" gorm:"column:dob;type:date"`
-	FullName    string `json:"full_name" gorm:"column:full_name;type:varchar(100)" validate:"required"`
-	CreatedAt   string `json:"-"`
-	UpdatedAt   string `json:"-"`
+	ID          int       `json:"id"`
+	Username    string    `json:"username" gorm:"column:username;type:varchar(20)" validate:"required"`
+	Email       string    `json:"email" gorm:"column:email;type:varchar(100)" validate:"required"`
+	Password    string    `json:"password" gorm:"column:password;type:varchar(72)" validate:"required"`
+	PhoneNumber string    `json:"phone_number" gorm:"column:phone_number;type:varchar(15)" validate:"required"`
+	Address     string    `json:"address" gorm:"column:address;type:text" `
+	Dob         string    `json:"dob" gorm:"column:dob;type:date"`
+	FullName    string    `json:"full_name" gorm:"column:full_name;type:varchar(100)" validate:"required"`
+	CreatedAt   time.Time `json:"-"`
+	UpdatedAt   time.Time `json:"-"`
 }
 
 func (*User) TableName() string {
